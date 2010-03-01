2a8e155c94d4afe22787c14e42a2329c9ef0a8bc
Malte Ubl <malubl@joose.(none)>
2010-03-01 21:50:41 +0100

    Port asyncEJS to node 0.30

--- test/test.js
+++ test/test.js
@@ -1,47 +1,47 @@
 var assert = require('assert'),
     sys = require("sys"),
     path = require('path'),
-    posix = require("posix");
+    fs = require("fs");
 
-var reference = posix.cat(path.join(__dirname, "fixtures/reference.html")).wait();
+var reference = fs.readFileSync(path.join(__dirname, "fixtures/reference.html"));
 
 (function testRender() {
 
   var te = require("../lib/asyncEJS").Engine();
 
-  var t1 = te.template("fixtures/template.js.html");
+  te.template("fixtures/template.js.html", function (t1) {
+    var templateResponse = t1({
+      hello: []
+    });
+    var body = ""
+    templateResponse.addListener("body", function (chunk) {
+      body += chunk;
+      sys.print(chunk);
+    });
 
-  var templateResponse = t1({
-    hello: []
-  });
-  var body = ""
-  templateResponse.addListener("body", function (chunk) {
-    body += chunk;
-    sys.print(chunk);
-  });
-
-  templateResponse.addListener("complete", function () {
-    if(body === reference) {
-      sys.puts("testRender: OK");
-    } else {
-      sys.puts("testRender: NOT OK")
-    }
-  });
+    templateResponse.addListener("complete", function () {
+      if(body === reference) {
+        sys.puts("testRender: OK");
+      } else {
+        sys.puts("testRender: NOT OK")
+      }
+    });
+  })
   
 })();
 
 (function testTemplateRoot() {
   
   var te = require('../lib/asyncEJS').Engine({templateRoot: path.join(__dirname, 'fixtures')});
-  var template = te.template('template_root_test.js.html');
-  var templateResponse = template({ hello: [] });
-  var body = ""
+  te.template('template_root_test.js.html', function (template) {
+    var templateResponse = template({ hello: [] });
+    var body = ""
   
-  templateResponse.addListener('body', function (c) { body += c; });
+    templateResponse.addListener('body', function (c) { body += c; });
   
-  process.addListener('exit', function () {
-    assert.equal(body, reference);
-    sys.puts('testTemplateRoot: passed');
+    process.addListener('exit', function () {
+      assert.equal(body, reference);
+      sys.puts('testTemplateRoot: passed');
+    });
   });
-  
 })();
